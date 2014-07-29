4775f956d1f770da9a53d454e34f9672a14939a0
Giaba90 <Gianluca@MacBook-di-Gianluca-Barranca.local>
2014-07-29 11:11:15 +0200

    add v3 and v4

---
+++ v4/README.md
@@ -0,0 +1,36 @@
+INTRO
+=====
+This is a One Page Parallax Template made in HTML5 and CSS3 with a Bootstrap framework.
+I made this project for personal scope, are you freedom to use this or to fork my project.
+
+Credits
+-------
+I thank these people for their work
+
++ icon [Font Awesome](http://fortawesome.github.io/Font-Awesome/)
++ font [bebas nueu regular](http://www.dafont.com/it/bebas-neue.font)
++ image:
+	- [Symic](https://www.flickr.com/photos/symic/3291665256/sizes/s/)
+	- [Sara Cimino](https://www.flickr.com/photos/saracimino/3916476954/sizes/n/)
+	- [See-ming Lee ](https://www.flickr.com/photos/seeminglee/4365575428/in/photolist-7DLHtS-bjKgjE-dkzgdq-bAQEi5-nwL8WU-Bns88-bXyZus-7GmQUb-9Q5fin-81qzkc-7MKd2G-8naL5J-a47cHV-4pYEv9-4pUuCR-4pUAv2-4pUvDi-y4HsS-83MGHs-hMzbzM-fusQjN-cDj1tu-bmRh2E-miKTv-cUx7pG-5cFxde-nQK1Vq-fehpTR-9WZQPX-e3eqvr-7mg2cv-cfZr7-dWz8ZN-nwKhQ4-9eomfE-5V7asX-9wVmXY-bXz8bA-ngBMZA-doLdQm-jAyAfr-4pYwHy-oYPan-onbNkM-8U1ADR-dJruXu-97VfF6-ddQ6Qw-e5hdiR-edZUNj)
+
+
+Copyright
+---------
+All my personal project are under GNU General Public License v2
+
+Copyright (C) 2014  Gianluca Barranca ([@giaba90](https://twitter.com/giaba90))
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
---
+++ v4/js/plugins.js
@@ -0,0 +1,68 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+// Place any jQuery/helper plugins in here.
+$( document ).ready(function() {
+/*$(".navigation a").bind("click", function () {
+$(".navigation a").removeClass("active");
+$(this).addClass("active");
+});
+*/
+	var ascensor = $('#ascensorBuilding').ascensor();
+	var ascensorInstance = $('#ascensorBuilding').data('ascensor');
+	$(".navigation li").click(function(event, index) {
+				ascensorInstance.scrollToFloor($(this).index());
+			});
+
+			$(".navigation li:eq("+ ascensor.data("current-floor") +")").addClass("selected");
+
+			ascensor.on("scrollStart", function(event, floor){
+				$(".navigation li").removeClass("selected");
+				$(".navigation li:eq("+floor.to+")").addClass("selected");
+			});
+
+      $(".direction").click(function() {
+        ascensorInstance.scrollToDirection($(this).data("direction"));
+      });
+
+function initialize() {
+            var map_canvas = document.getElementById('map_canvas');
+            var map_options = {
+              center: new google.maps.LatLng(37.5687, -85.7347),
+              zoom: 14,
+              mapTypeId: google.maps.MapTypeId.ROADMAP
+            }
+            var map = new google.maps.Map(map_canvas, map_options)
+          }
+         google.maps.event.addDomListener(window, 'load', initialize);
+
+$('#skill').mouseenter(function() {
+  $('#skill_sx').css('visibility','visible').addClass('bounceInLeft animated');
+  $('#skill_dx').css('visibility','visible').addClass('bounceInRight animated');
+});
+
+$('#address').mouseenter(function() {
+  $('#contact_container').css('visibility','visible').addClass('bounceInUp animated');
+});
+
+});
